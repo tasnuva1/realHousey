8ebefb3b9064b0575ac4ea37de770b9e1f3a1237
tasnuva1 <fahmidamashura@gmail.com>
2020-10-08 21:15:31 +0600

    search is working properly

--- src/Components/HouseSearchList/HouseSearchList.jsx
+++ src/Components/HouseSearchList/HouseSearchList.jsx
@@ -1,28 +1,35 @@
-import React from "react";
-import "./HouseSearchList.scss";
-import Heading from "../Heading/Heading";
-import Houses from "../../Components/Houses/Houses";
+import React from 'react';
+import './HouseSearchList.scss';
+import Heading from '../Heading/Heading';
+import Houses from '../../Components/Houses/Houses';
 
 const HouseSearchList = ({ houses }) => {
   if (houses.length === 0) {
+    console.log(houses.length);
     return (
-      <div className="house-search-ability">
-        <Heading heading="heading3">
+      <div className='house-search-ability'>
+        <Heading heading='heading3'>
           Unfortunately, No Houses Matched Your Search Results.
         </Heading>
       </div>
     );
-  }
-
-  return (
-    <div className="list-item-boxx">
-      <div className="list-item--box">
-        {houses.map((item) => {
-          return <Houses key={item.id} house={item} />;
-        })}
+  } else {
+    return (
+      <div className='list-item-boxx'>
+        <div className='list-item--box'>
+          {houses.map((item) => {
+            return <Houses key={item.id} house={item} />;
+          })}
+        </div>
       </div>
-    </div>
-  );
+
+      // <div className='house-search-ability'>
+      //   <Heading heading='heading3'>
+      //     Unfortunately, No Houses Matched Your Search Results.
+      //   </Heading>
+      // </div>
+    );
+  }
 };
 
 export default HouseSearchList;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,19 +1,19 @@
-import React from "react";
-import "./Home.style.scss";
+import React from 'react';
+import './Home.style.scss';
 
-import Main from "../../Components/Main/Main.Component";
-import AboutUs from "../../Components/AboutUs/AboutUs";
-import SmallCards from "../SmallCards/SmallCards";
-import Works from "../Works/HowItWorks";
-import Cards from "../Cards/Cards";
-import RecentListItem from "../RecentListItem/RecentListItem";
-import Testimonials from "../Testimonials/Testimonials";
-import Footer from "../../Components/Footer/Footer";
+import Main from '../../Components/Main/Main.Component';
+import AboutUs from '../../Components/AboutUs/AboutUs';
+import SmallCards from '../SmallCards/SmallCards';
+import Works from '../Works/HowItWorks';
+import Cards from '../Cards/Cards';
+import RecentListItem from '../RecentListItem/RecentListItem';
+import Testimonials from '../Testimonials/Testimonials';
+import Footer from '../../Components/Footer/Footer';
 
 function Home() {
   return (
-    <div className="home-full-hight">
-      <div className="home-main-content">
+    <div className='home-full-hight'>
+      <div className='home-main-content'>
         <Main />
         <AboutUs />
         <SmallCards />
@@ -22,7 +22,7 @@ function Home() {
         <RecentListItem />
         <Testimonials />
       </div>
-      <div className="home-footer">
+      <div className='home-footer'>
         <Footer />
       </div>
     </div>
